65cfb3dab4b16465f2e9d2732ada080378c13953
Rajesh <javabyrajesh1@gmail.com>
2021-05-30 18:00:11 +0530

    sts app

---
+++ src/test/java/com/rajesh/nit/SpringbootGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
